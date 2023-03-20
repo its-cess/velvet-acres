817a4e1c904c25e88bb347e4f5d65ff3444c9b81
Cecily Toro <torocecily@gmail.com>
2023-03-19 21:07:09 -0400

    fix: styling and col spacing

--- src/lib/base/GridContainer.svelte
+++ src/lib/base/GridContainer.svelte
@@ -1,3 +1,3 @@
-<div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 gap-y-3 px-4 mt-8">
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-3 px-4 mt-8">
 	<slot />
 </div>

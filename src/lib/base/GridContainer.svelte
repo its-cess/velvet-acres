a02aed8eef442512c1ffcd203317abb7a44080ca
Cecily Toro <torocecily@gmail.com>
2023-03-20 13:57:24 -0400

    fix styling on grid container. add max-width

--- src/lib/base/GridContainer.svelte
+++ src/lib/base/GridContainer.svelte
@@ -1,3 +1,5 @@
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-3 px-4 mt-8">
+<div
+	class="container mx-auto max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-3 px-4 mt-8"
+>
 	<slot />
 </div>

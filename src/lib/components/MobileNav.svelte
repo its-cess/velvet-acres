b6b3e73a85ce0d32b694dfe63cafe4b3701f7423
Cecily Toro <torocecily@gmail.com>
2023-04-23 13:35:34 -0400

    add: responsive mobileNav component

---
+++ src/lib/components/MobileNav.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	import Hamburger from '$lib/icons/Hamburger.svelte';
+
+	const dispatch = createEventDispatcher();
+	export let isMobileNavShowing = false;
+</script>
+
+<nav class="flex flex-row justify-between bg-green text-cream font-playfair">
+	<div>
+		<button on:click={() => dispatch('toggle')}>
+			<Hamburger height={32} width={32} />
+		</button>
+		{#if isMobileNavShowing}
+			<div class="absolute right-0 top-[3.5rem] z-40 w-full bg-green bg-opacity-90 text-right p-5">
+				<ul class="mr-8">
+					<li><a href="/horses" on:click={() => dispatch('toggle')}>Our Horses</a></li>
+					<li><a href="/facility" on:click={() => dispatch('toggle')}>Facilities</a></li>
+					<li><a href="/services" on:click={() => dispatch('toggle')}>Services</a></li>
+					<li><a href="/contact" on:click={() => dispatch('toggle')}>Contact Us</a></li>
+				</ul>
+			</div>
+		{/if}
+	</div>
+</nav>
+
+<style lang="postcss">
+	ul li a {
+		@apply text-xl font-lato text-cream;
+	}
+
+	ul li a:hover {
+		@apply text-tan text-2xl font-bold;
+	}
+</style>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,13 +1,37 @@
-<nav class="flex flex-row justify-between px-7 py-3 bg-green lato text-cream">
+<script>
+	import MobileNav from './MobileNav.svelte';
+
+	let isMobileNavOpen = false;
+</script>
+
+<nav class="flex flex-row justify-between items-center px-7 py-3 bg-green text-cream font-playfair">
 	<div>
 		<a href="/">
-			<h2 class="text-2xl playfair hover:text-dark-green">Velvet Acres</h2>
+			<h2 class="text-2xl playfair hover:text-dark-green hover:text-3xl">Velvet Acres</h2>
 		</a>
 	</div>
-	<div class="flex gap-10 text-xl">
-		<a class="hover:text-tan" href="/facility">Facilities</a>
-		<a class="hover:text-tan" href="/services">Services</a>
-		<a class="hover:text-tan" href="/horses">Our Horses</a>
-		<a class="hover:text-tan" href="/contact">Contact Us</a>
+	<ul class="hidden md:flex gap-10">
+		<li><a href="/horses">Our Horses</a></li>
+		<li><a href="/facility">Facilities</a></li>
+		<li><a href="/services">Services</a></li>
+		<li><a href="/contact">Contact Us</a></li>
+	</ul>
+
+	<!-- HAMBURGER MENU FOR MOBILE -->
+	<div class="block md:hidden">
+		<MobileNav
+			isMobileNavShowing={isMobileNavOpen}
+			on:toggle={() => (isMobileNavOpen = !isMobileNavOpen)}
+		/>
 	</div>
 </nav>
+
+<style lang="postcss">
+	ul li a {
+		@apply text-xl font-lato text-cream;
+	}
+
+	ul li a:hover {
+		@apply text-tan text-2xl font-bold;
+	}
+</style>

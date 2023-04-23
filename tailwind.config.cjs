6ac57e050f1495c36b6f9e8a8f5e1b9930dd109e
Cecily Toro <torocecily@gmail.com>
2023-04-23 13:31:03 -0400

    add: portableText, update: tailwind configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,25 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
-		colors: {
-			'dark-green': '#2C3639',
-			'green': '#3F4E4F',
-			'tan': '#A27B5C',
-			'cream': '#DCD7C9'
-		}
+		fontFamily: {
+			lato: ['Lato', 'sans-serif'],
+			playfair: ['Playfair Display', 'serif']
+		},
+		extend: {
+			colors: {
+				'dark-green': '#2C3639',
+				'green': '#3F4E4F',
+				'tan': '#A27B5C',
+				'cream': '#DCD7C9'
+			},
+			listStyleImage: {
+				horseshoe: 'url("images/horseshoe.png")'
+			},
+			zIndex: {
+				mobileNav: 91,
+				navBar: 90
+			}
+		},
 	},
 	plugins: []
 };

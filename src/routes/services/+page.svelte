fcd5dfb74d18477da595ae7c0a19a9d24765b455
Cecily Toro <torocecily@gmail.com>
2023-04-23 13:34:52 -0400

    add: additionalService schema to FE w/portableText

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,26 +1,43 @@
 <script>
 	import Heading from '$lib/base/Heading.svelte';
 	import BaseCard from '$lib/base/BaseCard.svelte';
-	import GridContainer from '$lib/base/GridContainer.svelte';
-	import { onMount } from 'svelte';
+
+	import { PortableText } from '@portabletext/svelte';
+	import DefaultListItem from '$lib/components/DefaultListItem.svelte';
+	import AdditionalServices from './AdditionalServices.svelte';
 
 	export let data;
 	export let services;
+	export let additionalServices;
 	services = data.services;
-	console.log(services);
-	// console.log(services[0].description[0].children[0].text);
+	additionalServices = data.additionalServices;
 </script>
 
-<Heading>Services</Heading>
-<div class="flex flex-col md:flex-row max-w-3xl mx-auto gap-10">
-	{#each services as service (service._id)}
-		<BaseCard>
-			<Heading secondary tertiary>{service.name}</Heading>
-			<span class="text-tan text-xl">${service.price}/month</span>
-		</BaseCard>
-	{/each}
-</div>
-<Heading secondary>Additional Services</Heading>
-<GridContainer>
-	<p>Add a BaseCard component and map over the Additional Services provided here.</p>
-</GridContainer>
+{#if services}
+	<Heading>Services</Heading>
+	<div class="flex flex-col md:flex-row max-w-3xl mx-auto gap-10">
+		{#each services as service (service._id)}
+			<BaseCard>
+				<Heading secondary tertiary>{service.name}</Heading>
+				<span class="text-tan text-xl">${service.price}/month</span>
+				<div class="text-left pt-3 text-xl">
+					<PortableText
+						value={service.description}
+						components={{
+							listItem: {
+								bullet: DefaultListItem,
+								normal: DefaultListItem
+							}
+						}}
+					/>
+				</div>
+			</BaseCard>
+		{/each}
+	</div>
+{:else}
+	<h3>Please check back later to see our services!</h3>
+{/if}
+
+{#if additionalServices}
+	<AdditionalServices {additionalServices} />
+{/if}

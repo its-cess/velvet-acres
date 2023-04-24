f0ebba0881348985ebb31d1235140bd688e9df33
Cecily Toro <torocecily@gmail.com>
2023-04-24 17:01:19 -0400

    env variables

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 import { PUBLIC_SANITY_STUDIO_PROJECT_ID } from '$env/static/public';
 
-const sanityClient = createClient({
+export const sanityClient = createClient({
 	projectId: PUBLIC_SANITY_STUDIO_PROJECT_ID,
 	dataset: 'production',
 	apiVersion: '2023-03-19',
